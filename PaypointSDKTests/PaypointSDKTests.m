5f4521fe6863cab5e001955d50d680d3241fa63d
Rob Nash <rob@robnash.me>
2015-04-10 14:19:50 +0100

    Added unit test for validating luhn test.

--- PaypointSDKTests/PaypointSDKTests.m
+++ PaypointSDKTests/PaypointSDKTests.m
@@ -8,33 +8,34 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import <PaypointSDK/PPOLuhn.h>
 
 @interface PaypointSDKTests : XCTestCase
-
+@property (nonatomic, strong) NSArray *pans;
 @end
 
 @implementation PaypointSDKTests
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    self.pans = @[
+                  @"9900000000005159", //with generate an authorization result
+                  @"9900000000005282", //will generate a decline result
+                  @"9900000000000168", //will return a valid response but wait 61 seconds
+                  @"9900000000010407" //will return an internal server error
+                  ];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    self.pans = nil;
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testLuhn {
+    for (NSString *pan in self.pans) {
+        NSAssert([PPOLuhn validateString:pan], @"Luhn check failed");
+    }
 }
 
 @end
