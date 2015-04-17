d056c9ebee6f35cb47c727d32a07ee4929ce8749
Rob Nash <rob@robnash.me>
2015-04-17 11:54:05 +0100

    Created new file

---
+++ PaypointSDK/PPOBaseURLManager.h
@@ -0,0 +1,13 @@
+//
+//  PPOBaseURLManager.h
+//  Paypoint
+//
+//  Created by Robert Nash on 17/04/2015.
+//  Copyright (c) 2015 Paypoint. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PPOBaseURLManager : NSObject
+
+@end
---
+++ PaypointSDK/PPOBaseURLManager.m
@@ -0,0 +1,13 @@
+//
+//  PPOBaseURLManager.m
+//  Paypoint
+//
+//  Created by Robert Nash on 17/04/2015.
+//  Copyright (c) 2015 Paypoint. All rights reserved.
+//
+
+#import "PPOBaseURLManager.h"
+
+@implementation PPOBaseURLManager
+
+@end
