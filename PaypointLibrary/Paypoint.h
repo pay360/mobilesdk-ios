5c55a9698bc2fc40fb643b14fe74ca681e924e2c
Rob Nash <rob@robnash.me>
2015-04-22 16:16:44 +0100

    Building framework from static library.

---
+++ PaypointLibrary/Paypoint.h
@@ -0,0 +1,20 @@
+//
+//  Paypoint.h
+//  Paypoint
+//
+//  Created by Robert Nash on 22/04/2015.
+//  Copyright (c) 2015 Paypoint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <PaypointSDK/PPOPaymentManager.h>
+#import <PaypointSDK/PPOLuhn.h>
+#import <PaypointSDK/PPOPayment.h>
+#import <PaypointSDK/PPOCreditCard.h>
+#import <PaypointSDK/PPOCredentials.h>
+#import <PaypointSDK/PPOTransaction.h>
+#import <PaypointSDK/PPOBillingAddress.h>
+#import <PaypointSDK/PPOError.h>
+#import <PaypointSDK/PPOOutcome.h>
+#import <PaypointSDK/PPOPaymentBaseURLManager.h>
