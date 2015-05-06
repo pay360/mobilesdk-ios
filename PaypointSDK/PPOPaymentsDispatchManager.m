b0106997ba586d4adb2e5ea8595002327c4f4128
Rob Nash <robnash@paypoint.com>
2015-05-06 15:27:04 +0100

    Now showing acs view.

--- PaypointSDK/PPOPaymentsDispatchManager.m
+++ PaypointSDK/PPOPaymentsDispatchManager.m
@@ -7,11 +7,12 @@
 //
 
 #import "PPOPaymentsDispatchManager.h"
+#import "PPOWebViewController.h"
 #import "PPOPayment.h"
 #import "PPOCredentials.h"
 #import "PPOErrorManager.h"
 
-@interface PPOPaymentsDispatchManager () <NSURLSessionTaskDelegate>
+@interface PPOPaymentsDispatchManager () <NSURLSessionTaskDelegate, PPOWebViewControllerDelegate>
 @property (nonatomic, strong, readwrite) NSOperationQueue *payments;
 @property (nonatomic, strong) NSURLSession *session;
 @end
@@ -38,9 +39,7 @@
     
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
     
-    NSURLSessionDataTask *task;
-    
-    task = [self.session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *networkError) {
+    NSURLSessionDataTask *task = [self.session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *networkError) {
         
         id json;
         NSError *invalidJSON;
@@ -49,30 +48,101 @@
             json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&invalidJSON];
         }
         
-        NSError *error;
-        PPOOutcome *outcome;
-        
         if (invalidJSON) {
-            error = [PPOErrorManager errorForCode:PPOErrorServerFailure];
-        } else if (json) {
-            outcome = [[PPOOutcome alloc] initWithData:json];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+                completion(nil, [PPOErrorManager errorForCode:PPOErrorServerFailure]);
+            });
+            return;
+        }
+        
+        id value = [json objectForKey:@"threeDSRedirect"];
+        if ([value isKindOfClass:[NSDictionary class]]) {
+            NSString *acsURLString = [value objectForKey:@"acsUrl"];
+            if ([acsURLString isKindOfClass:[NSString class]]) {
+                NSString *md = [value objectForKey:@"md"];
+                NSString *pareq = [value objectForKey:@"pareq"];
+                NSNumber *sessionTimeout = [value objectForKey:@"sessionTimeout"];
+                NSTimeInterval secondsTimeout = sessionTimeout.doubleValue/1000;
+                NSString *termUrlString = [value objectForKey:@"termUrl"];
+                NSURL *termURL = [NSURL URLWithString:termUrlString];
+                NSURL *acsURL = [NSURL URLWithString:acsURLString];
+                if (acsURL) {
+                    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:acsURL];
+                    [request setHTTPMethod:@"POST"];
+                    
+                    NSString *string = [NSString stringWithFormat:@"PaReq=%@&MD=%@&TermUrl=%@", [PPOPaymentsDispatchManager urlencode:pareq], [PPOPaymentsDispatchManager urlencode:md], termURL];
+                    NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding];
+                    
+                    [request setHTTPBody:data];
+                    
+                    PPOWebViewController *controller = [[PPOWebViewController alloc] init];
+                    controller.delegate = self;
+                    controller.request = request;
+                    
+                    UINavigationController *navCon = [[UINavigationController alloc] initWithRootViewController:controller];
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        [[[UIApplication sharedApplication] keyWindow].rootViewController presentViewController:navCon animated:YES completion:nil];
+                    });
+                } else {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+                        completion(nil, [PPOErrorManager errorForCode:PPOErrorProcessingThreeDSecure]);
+                    });
+                }
+            }
+            return;
+        }
+        
+        if (json) {
+            NSError *error;
+            PPOOutcome *outcome = [[PPOOutcome alloc] initWithData:json];
             if (outcome.isSuccessful == NO) {
                 PPOErrorCode code = [PPOErrorManager errorCodeForReasonCode:outcome.reasonCode.integerValue];
                 error = [PPOErrorManager errorForCode:code];
             }
-        } else if (networkError) {
-            error = networkError;
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+                completion(outcome, error);
+            });
         }
         
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
-            completion(outcome, error);
-        });
+        if (networkError) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+                completion(nil, networkError);
+            });
+        } else {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+                completion(nil, [PPOErrorManager errorForCode:PPOErrorUnknown]);
+            });
+        }
         
     }];
     
     [task resume];
     
+}
+
++(NSString *)urlencode:(NSString*)string {
+    NSMutableString *output = [NSMutableString string];
+    const unsigned char *source = (const unsigned char *)[string UTF8String];
+    unsigned long sourceLen = strlen((const char *)source);
+    for (int i = 0; i < sourceLen; ++i) {
+        const unsigned char thisChar = source[i];
+        if (thisChar == ' '){
+            [output appendString:@"+"];
+        } else if (thisChar == '.' || thisChar == '-' || thisChar == '_' || thisChar == '~' ||
+                   (thisChar >= 'a' && thisChar <= 'z') ||
+                   (thisChar >= 'A' && thisChar <= 'Z') ||
+                   (thisChar >= '0' && thisChar <= '9')) {
+            [output appendFormat:@"%c", thisChar];
+        } else {
+            [output appendFormat:@"%%%02X", thisChar];
+        }
+    }
+    return output;
 }
 
 - (void)URLSession:(NSURLSession *)session didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
@@ -83,5 +153,9 @@
     completionHandler(NSURLSessionAuthChallengeUseCredential, credential);
     
 }
+
+#pragma mark - PPOWebViewController
+
+
 
 @end
