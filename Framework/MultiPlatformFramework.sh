b59596e4e8bad524aa3d3f4ed9f234561714e5f1
Rob Nash <robnash@paypoint.com>
2015-05-06 15:01:05 +0100

    Removed some redundant schemes.  Added info.plist for framework to assist with versioning later. Created bundle and move web view xib into bundle.  Updated scripts to product a neat framework that includes the bundle.

--- Framework/MultiPlatformFramework.sh
+++ Framework/MultiPlatformFramework.sh
@@ -1,93 +1,64 @@
 set -e
-set -x 
-# If we're already inside this script then die
-if [ -n "$RW_MULTIPLATFORM_BUILD_IN_PROGRESS" ]; then
+set +u
+# Avoid recursively calling this script.
+if [[ $SF_MASTER_SCRIPT_RUNNING ]]
+then
 exit 0
 fi
-export RW_MULTIPLATFORM_BUILD_IN_PROGRESS=1
+set -u
+export SF_MASTER_SCRIPT_RUNNING=1
 
-RW_FRAMEWORK_NAME=${PROJECT_NAME}
-RW_INPUT_STATIC_LIB="lib${PROJECT_NAME}.a"
-RW_FRAMEWORK_LOCATION="${BUILT_PRODUCTS_DIR}/Paypoint.framework"
-#RW_FRAMEWORK_LOCATION="${BUILT_PRODUCTS_DIR}/${RW_FRAMEWORK_NAME}.framework"
+SF_TARGET_NAME=${PROJECT_NAME}
+SF_EXECUTABLE_PATH="lib${SF_TARGET_NAME}.a"
+SF_WRAPPER_NAME="${SF_TARGET_NAME}.framework"
+SF_RESOURCES_NAME="${SF_TARGET_NAME}Resources.bundle"
 
-function build_static_library {
-# Will rebuild the static library as specified
-#     build_static_library sdk
-xcrun xcodebuild -project "${PROJECT_FILE_PATH}" \
--target "${TARGET_NAME}" \
--configuration "${CONFIGURATION}" \
--sdk "${1}" \
-ONLY_ACTIVE_ARCH=NO \
-BUILD_DIR="${BUILD_DIR}" \
-OBJROOT="${OBJROOT}" \
-BUILD_ROOT="${BUILD_ROOT}" \
-SYMROOT="${SYMROOT}" $ACTION
-}
+# The following conditionals come from
+# https://github.com/kstenerud/iOS-Universal-Framework
 
-function make_fat_library {
-# Will smash 2 static libs together
-#     make_fat_library in1 in2 out
-echo xcrun lipo -create "${1}" "${2}" -output "${3}" >&1
-
-xcrun lipo -create "${1}" "${2}" -output "${3}"
-}
-
-# 1 - Extract the platform (iphoneos/iphonesimulator) from the SDK name
-if [[ "$SDK_NAME" =~ ([A-Za-z]+) ]]; then
-RW_SDK_PLATFORM=${BASH_REMATCH[1]}
+if [[ "$SDK_NAME" =~ ([A-Za-z]+) ]]
+then
+SF_SDK_PLATFORM=${BASH_REMATCH[1]}
 else
 echo "Could not find platform name from SDK_NAME: $SDK_NAME"
 exit 1
 fi
 
-# 2 - Extract the version from the SDK
-if [[ "$SDK_NAME" =~ ([0-9]+.*$) ]]; then
-RW_SDK_VERSION=${BASH_REMATCH[1]}
+if [[ "$SDK_NAME" =~ ([0-9]+.*$) ]]
+then
+SF_SDK_VERSION=${BASH_REMATCH[1]}
 else
 echo "Could not find sdk version from SDK_NAME: $SDK_NAME"
 exit 1
 fi
 
-# 3 - Determine the other platform
-if [ "$RW_SDK_PLATFORM" == "iphoneos" ]; then
-RW_OTHER_PLATFORM=iphonesimulator
+if [[ "$SF_SDK_PLATFORM" = "iphoneos" ]]
+then
+SF_OTHER_PLATFORM=iphonesimulator
 else
-RW_OTHER_PLATFORM=iphoneos
+SF_OTHER_PLATFORM=iphoneos
 fi
 
-# 4 - Find the build directory
-if [[ "$BUILT_PRODUCTS_DIR" =~ (.*)$RW_SDK_PLATFORM$ ]]; then
-RW_OTHER_BUILT_PRODUCTS_DIR="${BASH_REMATCH[1]}${RW_OTHER_PLATFORM}"
+if [[ "$BUILT_PRODUCTS_DIR" =~ (.*)$SF_SDK_PLATFORM$ ]]
+then
+SF_OTHER_BUILT_PRODUCTS_DIR="${BASH_REMATCH[1]}${SF_OTHER_PLATFORM}"
 else
-echo "Could not find other platform build directory."
+echo "Could not find platform name from build products directory: $BUILT_PRODUCTS_DIR"
 exit 1
 fi
 
 # Build the other platform.
-build_static_library "${RW_OTHER_PLATFORM}${RW_SDK_VERSION}"
+xcodebuild -project "${PROJECT_FILE_PATH}" -target "${TARGET_NAME}" -configuration "${CONFIGURATION}" -sdk ${SF_OTHER_PLATFORM}${SF_SDK_VERSION} BUILD_DIR="${BUILD_DIR}" OBJROOT="${OBJROOT}" BUILD_ROOT="${BUILD_ROOT}" SYMROOT="${SYMROOT}" $ACTION
 
-# If we're currently building for iphonesimulator, then need to rebuild
-#   to ensure that we get both i386 and x86_64
-if [ "$RW_SDK_PLATFORM" == "iphonesimulator" ]; then
-build_static_library "${SDK_NAME}"
-fi
+# Smash the two static libraries into one fat binary and store it in the .framework
+lipo -create "${BUILT_PRODUCTS_DIR}/${SF_EXECUTABLE_PATH}" "${SF_OTHER_BUILT_PRODUCTS_DIR}/${SF_EXECUTABLE_PATH}" -output "${BUILT_PRODUCTS_DIR}/${SF_WRAPPER_NAME}/${SF_TARGET_NAME}"
 
-# Join the 2 static libs into 1 and push into the .framework
-make_fat_library "${BUILT_PRODUCTS_DIR}/${RW_INPUT_STATIC_LIB}" \
-"${RW_OTHER_BUILT_PRODUCTS_DIR}/${RW_INPUT_STATIC_LIB}" \
-"${RW_FRAMEWORK_LOCATION}/Versions/A/${RW_FRAMEWORK_NAME}"
+# Copy the binary to the other architecture folder to have a complete framework in both.
+cp -a "${BUILT_PRODUCTS_DIR}/${SF_WRAPPER_NAME}/${SF_TARGET_NAME}" "${SF_OTHER_BUILT_PRODUCTS_DIR}/${SF_WRAPPER_NAME}/${SF_TARGET_NAME}"
 
-# Ensure that the framework is present in both platform's build directories
-cp -a "${RW_FRAMEWORK_LOCATION}/Versions/A/${RW_FRAMEWORK_NAME}" \
-"${RW_OTHER_BUILT_PRODUCTS_DIR}/Paypoint.framework/Versions/A/${RW_FRAMEWORK_NAME}"
+rm -rf  "build/${SF_TARGET_NAME}.framework"
 
-# Copy the framework to the user's desktop
-rm -rf  "build/${RW_FRAMEWORK_NAME}.framework"
+ditto "${BUILT_PRODUCTS_DIR}/${SF_RESOURCES_NAME}" "${BUILT_PRODUCTS_DIR}/${SF_TARGET_NAME}.framework/${SF_RESOURCES_NAME}"
+ditto "${BUILT_PRODUCTS_DIR}/${SF_TARGET_NAME}.framework" "build/${SF_TARGET_NAME}.framework"
 
-ditto "${BUILT_PRODUCTS_DIR}/Paypoint.framework" "build/${RW_FRAMEWORK_NAME}.framework"
-ditto -cz "build/${RW_FRAMEWORK_NAME}.framework" "build/${RW_FRAMEWORK_NAME}.framework.zip"
-#cp -a "${RW_FRAMEWORK_LOCATION}/Versions/A/${RW_FRAMEWORK_NAME}" \
-#"build/${RW_FRAMEWORK_NAME}.framework"
-
-#open "${BUILT_PRODUCTS_DIR}/Paypoint.framework"
+ditto -cz "build/${SF_TARGET_NAME}.framework" "build/${SF_TARGET_NAME}.framework.zip"
