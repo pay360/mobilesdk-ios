db4aeef4612013eac1cc3a22906d74d38ccde7a0
Rob Nash <robnash@paypoint.com>
2015-06-18 11:04:15 +0100

    Added file.

---
+++ PaypointSDK/Public/PPOCard.h
@@ -0,0 +1,27 @@
+//
+//  PPOCard.h
+//  Paypoint
+//
+//  Created by Robert Nash on 07/04/2015.
+//  Copyright (c) 2015 Paypoint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/*!
+@class PPOCard
+@discussion An instance of this class represents a credit card.
+ */
+@interface PPOCard : NSObject
+@property (nonatomic, strong) NSString *pan;
+@property (nonatomic, strong) NSString *cvv;
+@property (nonatomic, strong) NSString *expiry;
+@property (nonatomic, strong) NSString *cardHolderName;
+
+/*!
+@discussion A convenience method for building a plist of assigned values.
+@return A plist of assigned values. The NSDictionary instance will be valid for JSON serialisation using the NSJSONSerialization parser in Foundation.framework.
+ */
+-(NSDictionary*)jsonObjectRepresentation;
+
+@end
