4d26262f11ed674e360e8046b8de70dcca61046e
Rob Nash <robnash@paypoint.com>
2015-07-08 17:03:00 +0100

    Is safe to retry payment if user cancelled three d secure.

--- PaypointSDK/PPOErrorManager.m
+++ PaypointSDK/PPOErrorManager.m
@@ -424,6 +424,7 @@
     }
     
     return [@[
+              @(PPOPaymentErrorUserCancelledThreeDSecure),
               @(PPOPaymentErrorClientTokenExpired),
               @(PPOPaymentErrorClientTokenInvalid),
               @(PPOPaymentErrorUnauthorisedRequest),
